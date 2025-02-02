c5bb5ee6ef7d563d311b13ec4a5104938dbbecf3
unknown <imthedank69@gmail.com>
2025-02-02 22:17:50 +0700

    change process

--- status.php
+++ status.php
@@ -3,7 +3,7 @@ require_once 'app/app.php';
 $App = new App();
 
 if(isset($_GET['id'])){
-    $status = $_GET['status'];
-    $id = $_GET['id'];
+    $status = $_POST['status'];
+    $id = $_POST['id'];
     $App->changeStatus($id, $status);
 }
